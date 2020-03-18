138e4b59be09c27a3d785a48d5e4b7c2e43b2ab0
gmanriqe <gmanriqe@gmail.com>
2020-03-18 01:31:34 -0500

    Html Semantico y Modificando Estilos

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,56 @@
 var gulp = require('gulp');
-// var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create();
+var browserslist= require('browserslist');
 var autoprefixer = require("gulp-autoprefixer");
 var concat = require('gulp-concat'); /* concatenando css */
 var cleanCSS = require("gulp-clean-css"); /* minificando y limpiando comentarios css */
 var jsmin = require("gulp-jsmin"); /* minifico js */
 var rename = require("gulp-rename"); /* renombro archivos */
+
+function style(){
+    return gulp.src([
+            "css/menu-animate.css",
+            "css/style.css",
+            "css/media-queries.css"
+        ])
+        .pipe(autoprefixer(
+            {
+                'overrideBrowserslist': ['last 2 versions'] //https://github.com/browserslist/browserslist#full-list
+            }
+        ))
+        .pipe(cleanCSS())
+        .pipe(rename("style.min.css"))
+        .pipe(concat("style.min.css"))
+        .pipe(gulp.dest("css/public/"))
+        .pipe(browserSync.stream());
+}
+
+function javascript(){
+    return gulp.src("./js/app.js")
+        .pipe(jsmin())
+        .pipe(rename("app.min.js"))
+        .pipe(gulp.dest('js/public/'))
+        .pipe(browserSync.stream());
+}
+
+function watchFiles() {
+    gulp.watch('css/style.css', style);
+    gulp.watch('js/app.js', javascript);
+    gulp.watch('*.html').on('change', browserSync.reload);
+}
+
+function browser() {
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 8080
+    });
+}
+
+const watch = gulp.series(style, javascript, gulp.parallel(watchFiles, browser));
+exports.default = watch;
 
 // Save a reference to the `reload` method
 // Watch scss AND html files, doing different things with each.
@@ -22,34 +68,6 @@ var rename = require("gulp-rename"); /* renombro archivos */
 //     gulp.watch("./js/app.js").on("change", browserSync.reload);
 // });
 
-// tarea de nombre "css"
-gulp.task("css", function(){
-    gulp
-        .src([
-            "css/menu-animate.css",
-            "css/style.css",
-            "css/media-queries.css"
-        ])
-        .pipe(autoprefixer(
-            {
-                //https://github.com/browserslist/browserslist#full-list
-                browsers: ['last 2 versions']
-            }
-        ))
-        .pipe(cleanCSS())
-        .pipe(rename("style.min.css"))
-        .pipe(concat("style.min.css"))
-        .pipe(gulp.dest("css/public/"));
-});
-
-// tarea de nombre "js"
-gulp.task("js", function(){
-    gulp
-        .src("./js/app.js")
-        .pipe(jsmin())
-        .pipe(rename("app.min.js"))
-        .pipe(gulp.dest('js/public/'));
-});
 
 /** correr todas las tareas */
-gulp.task("default", gulp.parallel("css","js"));
+// gulp.task("default", gulp.parallel("css","js"));
